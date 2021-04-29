80b9d9d3440781397f55ee673523254ef71be8ee
Roman <r.rysev@tesonero.com>
2021-04-29 12:37:27 +0300

    доделал алгоритмы

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="main">
-    <p v-for="(item, id) in filteredData" :key="id">{{ item }}</p>
+    <input type="checkbox" name="Levels" v-model="withLevels" />
+    <label for="Levels">Режим подразделений</label>
+    <div v-for="(item, id) in filteredData" :key="id">
+      <p :class="{ list: item.list }">{{ item }}</p>
+    </div>
   </div>
 </template>
 
@@ -10,31 +14,142 @@ export default {
   data() {
     return {
       apiData: data,
+      withLevels: false,
     };
   },
   computed: {
     filteredData() {
-      let res = [{ label: "Выбрать всех" }]; // массив с результатом начинается с "Выбрать всех"
-      let container = {}; // сюда будем складывать результаты поиска пользователей
+      const structures = this.setParentIdForStructures(this.apiData.structures);
       const listsObject = {}; // сделаем объект из lists, чтобы ускорить поиск т.к. искать в них будем часто и список может быть большим
-
-      const structures = this.setParentIdForStructures();
 
       this.apiData.lists.forEach((item) => {
         listsObject[item.id] = item;
       });
 
-      /*this.apiData.structures.forEach((item) => {
-        if (item.children.length > 0) {
-          this.filterData(item.children, listsObject, item.id, container);
+      if (this.withLevels) {
+        return this.parseFiltredDataWithLevels(
+          this.filterDataWithLevels({
+            data: structures,
+            path: [],
+            listsObject: listsObject,
+            initial: [],
+          })
+        );
+      } else {
+        const filterDataResult = this.filterData({
+          data: structures,
+          listsObject: listsObject,
+          parentId: null,
+          container: {},
+        });
+        const res = [{ label: "Выбрать всех" }];
+        this.apiData.lists.forEach((list) => {
+          res.push(...filterDataResult[list.id]);
+        });
+        return res;
+      }
+    },
+  },
+  methods: {
+    setParentIdForStructures(structures) {
+      const data = structures.slice();
+      function recursive(data, parentId) {
+        data.forEach((item) => {
+          item.parentId = parentId;
+          if (item.children && item.children.length > 0) {
+            return recursive(item.children, item.id);
+          } else {
+            return;
+          }
+        });
+        return data;
+      }
+      return recursive(data, null, []);
+    },
+    filterData(payload) {
+      payload.data.forEach((element) => {
+        if (element.lists) {
+          // Полагаю, что поле "lists" есть только у пользователей и у пользователей нет других "children"
+          element.lists.forEach((listId) => {
+            if (payload.listsObject[listId]) {
+              if (!payload.container[listId]) {
+                payload.container[listId] = [
+                  Object.assign({ list: true }, payload.listsObject[listId]), // list: true - для выделения строчки в разметке и назнечения CSS класса
+                ];
+              }
+              payload.container[listId].push(
+                Object.assign({}, element, {
+                  parentId: payload.parentId,
+                  currentListId: listId,
+                }) // Добавляем копию т.к. иначе добавление пользователя в другой список перезапишет поле currentListId у всех экземпляров
+              );
+            }
+          });
+        } else if (element.children) {
+          this.filterData({
+            data: element.children,
+            listsObject: payload.listsObject,
+            parentId: element.id,
+            container: payload.container,
+          });
+        } else {
+          return;
         }
-      });*/
-      container = this.filterDataWithLevels(structures);
-      console.log(container);
+      });
+      return payload.container;
+    },
+    filterDataWithLevels(payload) {
+      const res = [...payload.path];
+      payload.data.forEach((element) => {
+        if (element.lists) {
+          element.lists.forEach((listId) => {
+            if (payload.listsObject[listId]) {
+              if (!res[res.length - 1].children[listId]) {
+                res[res.length - 1].children[listId] = [
+                  {
+                    id: element.id,
+                    label: element.label,
+                    parentId: element.parentId,
+                  },
+                ];
+              } else {
+                res[res.length - 1].children[listId].push({
+                  id: element.id,
+                  label: element.label,
+                  parentId: element.parentId,
+                });
+              }
+            }
+          });
+        } else if (element.children) {
+          const newPath = [
+            ...payload.path,
+            {
+              id: element.id,
+              label: element.label,
+              parentId: element.parentId,
+              children: {},
+            },
+          ];
+          payload.initial = this.filterDataWithLevels({
+            data: element.children,
+            path: newPath,
+            listsObject: payload.listsObject,
+            initial: payload.initial,
+          });
+        }
+      });
+      if (res.length > 1) {
+        payload.initial.push(res);
+      }
+      return payload.initial;
+    },
+    parseFiltredDataWithLevels(filterDataWithLevels) {
+      const res = [{ label: "Выбрать всех" }];
       this.apiData.lists.forEach((list) => {
-        res.push(list);
+        res.push(Object.assign({ list: true }, list)); // list: true - для выделения строчки в разметке и назнечения CSS класса
         const sourceTable = {};
-        container.forEach((item) => {
+        filterDataWithLevels.forEach((item) => {
           item.forEach((elem) => {
             if (Object.entries(elem.children).length > 0) {
               if (sourceTable[elem.parentId] !== undefined) {
@@ -69,110 +184,11 @@ export default {
       return res;
     },
   },
-  methods: {
-    setParentIdForStructures() {
-      const data = this.apiData.structures.slice();
-      function recursive(data, parentId) {
-        data.forEach((item) => {
-          item.parentId = parentId;
-          if (item.children && item.children.length > 0) {
-            return recursive(item.children, item.id);
-          } else {
-            return;
-          }
-        });
-        return data;
-      }
-      return recursive(data, null, []);
-    },
-    filterData(data, listsObject, parentId, container) {
-      data.forEach((element) => {
-        if (element.lists) {
-          // Полагаю, что поле "lists" есть только у пользователей и у пользователей нет других "children"
-          element.lists.forEach((listId) => {
-            if (listsObject[listId]) {
-              if (!container[listId]) {
-                container[listId] = [listsObject[listId]];
-              }
-              container[listId].push(
-                Object.assign({}, element, {
-                  parentId: parentId,
-                  currentListId: listId,
-                }) // Добавляем копию т.к. иначе добавление пользователя в другой список перезапишет поле currentListId у всех экземпляров
-              );
-            }
-            // Я не уверен, что правильно понял строчку в задании: "Массив элементов в итоговом списке должен получиться одноуровневым,
-            // без вложенности.", ведь на скриншоте есть вложенность сотрудников в списки.
-            // На всякий случай, чтобы сотрудники были вложены в списки, инструкции внутри этого forEach должны выглядеть так:
-            //
-            // if (listsObject[elem]) {
-            //   if (!container[elem]) {
-            //     container[elem] = listsObject[elem];
-            //     container[elem].children = [];
-            //   }
-            //   container[elem].children.push(
-            //     Object.assign({}, item, {
-            //       parentId: parentId,
-            //       currentListId: elem,
-            //       }
-            //     )
-            //   );
-            // }
-          });
-        } else if (element.children) {
-          this.filterData(element.children, listsObject, element.id, container);
-        } else {
-          return;
-        }
-      });
-    }, // Функция получилась "грязная" - модифицируется аргумент "container". В данном случае мне кажется, что это применимо
-    // т.к. "container" создавался только для этого и не будет использоваться нигде больше.
-
-    filterDataWithLevels(data, path = [], initial = []) {
-      const res = [...path];
-      data.forEach((element) => {
-        if (element.lists) {
-          element.lists.forEach((listId) => {
-            if (!res[res.length - 1].children[listId]) {
-              res[res.length - 1].children[listId] = [
-                {
-                  id: element.id,
-                  label: element.label,
-                  parentId: element.parentId,
-                },
-              ];
-            } else {
-              res[res.length - 1].children[listId].push({
-                id: element.id,
-                label: element.label,
-                parentId: element.parentId,
-              });
-            }
-          });
-        } else if (element.children) {
-          const newPath = [
-            ...path,
-            {
-              id: element.id,
-              label: element.label,
-              parentId: element.parentId,
-              children: {},
-            },
-          ];
-          initial = this.filterDataWithLevels(
-            element.children,
-            newPath,
-            initial
-          );
-        }
-      });
-      if (res.length > 1) {
-        initial.push(res);
-      }
-      return initial;
-    },
-  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.list {
+  background-color: cornflowerblue;
+}
+</style>
